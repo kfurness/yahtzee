67540b8ec917fd54746a6108117202ec5f7b6835
kelly <kelly.n.waite@gmail.com>
2017-03-02 13:43:44 -0700

    Refactor reduce codes

--- reduce.tests.js
+++ reduce.tests.js
@@ -4,6 +4,7 @@ var reduceExport = require('./reduce')
 let cases3oKind = [
   {expected: {ThreeKindOMG: 6}, msg: {category: 'ThreeOfKindOMG', value: 6}, state: {ThreeKindOMG: 0}},
   {expected: {ThreeKindOMG: 8}, msg: {category: 'ThreeOfKindOMG', value: 8}, state: {ThreeKindOMG: 0}},
+  {expected: {ThreeKindOMG: 18}, msg: {category: 'ThreeOfKindOMG', value: 18}, state: {ThreeKindOMG: 0}},
 ]
 
 test('should apply three of a kind correctly', t => {
